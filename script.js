b86c8b4b6eb8e2512eefbd8579fe61922875d75a
rdbarker <ryan.dean.barker@gmail.com>
2021-10-10 13:34:50 -0400

    added game board module that generates the grid.

---
+++ script.js
@@ -0,0 +1,13 @@
+const gameBoard = (()=>{
+    //generate game board
+    const gridSize = 3;
+    let gridArray = [];
+    for (let x=0;x<gridSize;x++){
+        let gridRow = [];
+        for (let y=0;y<gridSize;y++){
+            gridRow.push(Cell(x,y));
+        }
+        gridArray.push(gridRow);
+    }
+
+})();
